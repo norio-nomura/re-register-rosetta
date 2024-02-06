35b248665658bcc17d22edf1d1e3b34d2670eb3d
Norio Nomura <norio.nomura@gmail.com>
2024-02-06 23:31:23 +0900

    re-register-rosetta.sh: add some checks

--- re-register-rosetta.sh
+++ re-register-rosetta.sh
@@ -10,12 +10,21 @@ if [ $check -ne 3 ]; then
 fi
 
 echo "Rosetta is not correctly registered. Re-registering rosetta..."
-# mount binfmt_misc
-mount -v -t binfmt_misc none /proc/sys/fs/binfmt_misc &&
-# remove Rosetta
-echo -1 >/proc/sys/fs/binfmt_misc/rosetta &&
+# mount /proc/sys/fs/binfmt_misc
+[ -w /proc/sys/fs/binfmt_misc/register ] || mount -t binfmt_misc none /proc/sys/fs/binfmt_misc
+
+# mount /run/rosetta
+[ -d /run/rosetta ] || mkdir -p /run/rosetta
+[ -f /run/rosetta/rosetta ] || mount -t virtiofs ROSETTA /run/rosetta
+
+# remove Rosetta from binfmt_misc
+[ -f /proc/sys/fs/binfmt_misc/rosetta ] && echo -1 >/proc/sys/fs/binfmt_misc/rosetta
+
 # register Rosetta
-echo ':rosetta:M::\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x3e\x00:\xff\xff\xff\xff\xff\xfe\xfe\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff:/run/rosetta/rosetta:OCF'>/proc/sys/fs/binfmt_misc/register 2>/dev/null
+echo ':rosetta:M::\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x3e\x00:\xff\xff\xff\xff\xff\xfe\xfe\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff:/run/rosetta/rosetta:OCF'>/proc/sys/fs/binfmt_misc/register
+
+# umount /proc/sys/fs/binfmt_misc
+umount /proc/sys/fs/binfmt_misc
 
 # check if Rosetta is correctly re-registered.
 check=$(/usr/bin/cat /proc/self/cmdline|xargs -0 echo|wc -w)
